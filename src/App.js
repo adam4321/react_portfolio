1fc011b368fbbef3fad7b78e83d683a0ad46ce96
Adam Wright <adamjw321@gmail.com>
2018-11-26 03:01:35 -0800

    clean up lines

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 // import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/core/styles';
-
 import Drawer from '@material-ui/core/Drawer';
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
@@ -23,7 +22,6 @@ import Divider from '@material-ui/core/Divider';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-
 import logo from './Adam_logo.png';
 import skyline from './skyline.png';
 import pin from './maps-pin.png';
