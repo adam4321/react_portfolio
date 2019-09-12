dfd75a1e08122418882553f0e4e60cae6f703e47
Wright <wrighada@oregonstate.edu>
2019-09-11 18:36:50 -0700

    change prettier settings

--- src/temporaryDrawer.js
+++ src/temporaryDrawer.js
@@ -11,73 +11,79 @@ import icon from './images/x-icon.png';
 // The mobile menu drawer component
 
 class TemporaryDrawer extends React.Component {
-  state = {
-    left: false
-     };
+    state = {
+        left: false
+    };
 
-  toggleDrawer = (side, open) => () => {
-    this.setState({
-      [side]: open,
-    });
-  };
+    toggleDrawer = (side, open) => () => {
+        this.setState({
+            [side]: open
+        });
+    };
 
-  render() {
+    render() {
+        const sideList = (
+            <div className="drawer">
+                <List id="menu-list">
+                    <button
+                        className="button-styled-link"
+                        onClick={this.toggleDrawer('left', false)}
+                    >
+                        <ListItem id="close">
+                            <ListItemIcon id="close">
+                                <img src={icon} alt="x icon" id="x-icon" />
+                            </ListItemIcon>
+                            <p id="close-text">CLOSE MENU</p>
+                        </ListItem>
+                    </button>
+                    <Divider className="divider" />
+                    <a href="https://adamjwright.com/#stack">
+                        <ListItem id="projects-link">
+                            <p id="projects-link">Projects</p>
+                        </ListItem>
+                    </a>
+                    <Divider className="divider" />
+                    <a href="https://adamjwright.com/#footer">
+                        <ListItem id="about-link">
+                            <p id="about-link">About</p>
+                        </ListItem>
+                    </a>
+                    <Divider className="divider" />
+                    <a href="https://adamjwright.com/blog">
+                        <ListItem id="blog-link">
+                            <p id="blog-link">Blog</p>
+                        </ListItem>
+                    </a>
+                    <Divider className="divider" />
+                </List>
+            </div>
+        );
 
-    const sideList = (
-      <div className='drawer'>
-        <List id='menu-list'>
-          <button className='button-styled-link' onClick={this.toggleDrawer('left', false)}>
-            <ListItem id='close'>
-              <ListItemIcon id='close'>
-                <img src={icon} alt='x icon' id='x-icon'/>
-              </ListItemIcon >
-              <p id='close-text'>CLOSE MENU</p>
-            </ListItem>
-          </button>
-            <Divider className='divider'/>
-          <a href='https://adamjwright.com/#stack'>
-            <ListItem id='projects-link'>
-              <p id='projects-link'>Projects</p>
-            </ListItem>
-          </a>
-            <Divider className='divider' />
-          <a href='https://adamjwright.com/#footer'>
-            <ListItem id='about-link'>
-              <p id='about-link'>About</p>
-            </ListItem>
-          </a>
-            <Divider className='divider' />
-          <a href='https://adamjwright.com/blog'>
-            <ListItem id='blog-link'>
-              <p id='blog-link'>Blog</p>
-            </ListItem>
-          </a>
-            <Divider className='divider' />
-        </List>
-      </div>
-    );
-
-    return (
-      <div>
-          <div onClick={this.toggleDrawer('left', true)} id='menu-container'>
-            <img src={menu} alt='menu'  className={'menu'}/>
-              <p id='menu-text'>
-                  Menu
-              </p>
-          </div>
-        <Drawer open={this.state.left} onClose={this.toggleDrawer('left', false)}>
-          <div
-            tabIndex={0}
-            role="button"
-            onClick={this.toggleDrawer('left', false)}
-            onKeyDown={this.toggleDrawer('left', false)}
-          >
-            {sideList}
-          </div>
-        </Drawer>
-      </div>
-    );
-  }
+        return (
+            <div>
+                <div
+                    onClick={this.toggleDrawer('left', true)}
+                    id="menu-container"
+                >
+                    <img src={menu} alt="menu" className={'menu'} />
+                    <p id="menu-text">Menu</p>
+                </div>
+                <Drawer
+                    open={this.state.left}
+                    onClose={this.toggleDrawer('left', false)}
+                >
+                    <div
+                        tabIndex={0}
+                        role="button"
+                        onClick={this.toggleDrawer('left', false)}
+                        onKeyDown={this.toggleDrawer('left', false)}
+                    >
+                        {sideList}
+                    </div>
+                </Drawer>
+            </div>
+        );
+    }
 }
 
 // TemporaryDrawer.propTypes = {
@@ -86,4 +92,5 @@ class TemporaryDrawer extends React.Component {
 
 // @ts-ignore
 // export default withStyles(styles)(TemporaryDrawer);
+
 export default TemporaryDrawer;
