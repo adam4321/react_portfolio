5173ed3adc5218b71f9b273e7aa3b5d2ac061498
Adam Wright <adam@adamjwright.com>
2020-12-20 05:32:07 -0800

    Fix link in build

--- src/App.js
+++ src/App.js
@@ -286,14 +286,14 @@ function Album(props) {
                         </Link>
                         <Link
                             activeClass="active"
-                            to="Album-layout-6"
+                            to="jss6"
                             spy={true}
                             smooth={true}
                             offset={30}
                             duration={550}
                             delay={200}
                         >
-                            <a href="Album-layout-6" id="projects" className="header-link"> Projects </a>
+                            <a href="jss6" id="projects" className="header-link"> Projects </a>
                         </Link>
                     </div>
 
