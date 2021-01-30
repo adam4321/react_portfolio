c3d64faf4ec237be99482817d711efbbdfb40456
Adam Wright <adam@adamjwright.com>
2021-01-30 14:37:18 -0800

    refactor to function with hooks

--- src/temporaryDrawer.js
+++ src/temporaryDrawer.js
@@ -7,7 +7,7 @@
 
 // @ts-check
 
-import React from 'react';
+import React, { useState } from 'react';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
@@ -20,98 +20,93 @@ import { Link } from 'react-scroll';
 
 /* Mobile Drawer Component ------------------------------------------------- */
 
-class TemporaryDrawer extends React.Component {
-    state = {
-        left: false
+function TemporaryDrawer() {
+    const [left, setLeft] = useState(false);
+
+    const toggleDrawer = (side, open) => () => {
+        setLeft(open);
     };
 
-    toggleDrawer = (side, open) => () => {
-        this.setState({
-            [side]: open
-        });
-    };
+    const sideList = (
+        <div className="drawer">
+            
+            <List id="menu-list">
+                <button className="button-styled-link" onClick={toggleDrawer('left', false)}>
+                    <ListItem id="close">
+                        <ListItemIcon id="close">
+                            <img src={icon} alt="x icon" id="x-icon"/>
+                        </ListItemIcon>
+                        <p id="close-text">CLOSE MENU</p>
+                    </ListItem>
+                </button>
+                <Divider className="divider"/>
 
-    render() {
-        const sideList = (
-            <div className="drawer">
-                
-                <List id="menu-list">
-                    <button className="button-styled-link" onClick={this.toggleDrawer('left', false)}>
-                        <ListItem id="close">
-                            <ListItemIcon id="close">
-                                <img src={icon} alt="x icon" id="x-icon"/>
-                            </ListItemIcon>
-                            <p id="close-text">CLOSE MENU</p>
-                        </ListItem>
-                    </button>
-                    <Divider className="divider"/>
+                <Link
+                    activeClass="active"
+                    to="stack"
+                    spy={true}
+                    smooth={true}
+                    offset={0}
+                    duration={550}
+                    delay={200}
+                >
+                    <ListItem id="projects-link" onClick={toggleDrawer('left', false)}>
+                        <p id="projects-link">Projects</p>
+                    </ListItem>
+                </Link>
+                <Divider className="divider"/>
 
-                    <Link
-                        activeClass="active"
-                        to="stack"
-                        spy={true}
-                        smooth={true}
-                        offset={0}
-                        duration={550}
-                        delay={200}
-                    >
-                        <ListItem id="projects-link" onClick={this.toggleDrawer('left', false)}>
-                            <p id="projects-link">Projects</p>
-                        </ListItem>
-                    </Link>
-                    <Divider className="divider"/>
+                <Link
+                    activeClass="active"
+                    to="bottom-container"
+                    spy={true}
+                    smooth={true}
+                    offset={20}
+                    duration={550}
+                    delay={200}
+                >
+                    <ListItem id="about-link" onClick={toggleDrawer('left', false)}>
+                        <p id="about-link">About</p>
+                    </ListItem>
+                </Link>
+                <Divider className="divider"/>
 
-                    <Link
-                        activeClass="active"
-                        to="bottom-container"
-                        spy={true}
-                        smooth={true}
-                        offset={20}
-                        duration={550}
-                        delay={200}
-                    >
-                        <ListItem id="about-link" onClick={this.toggleDrawer('left', false)}>
-                            <p id="about-link">About</p>
-                        </ListItem>
-                    </Link>
-                    <Divider className="divider"/>
+                <a href="https://adamjwright.com/blog">
+                    <ListItem id="blog-link">
+                        <p id="blog-link">Blog</p>
+                    </ListItem>
+                </a>
+                <Divider className="divider"/>
+                <a href="https://adamjwright.com/blog/contact_me">
+                    <ListItem id="contact-me-link">
+                        <p id="contact-me-link">Contact Me</p>
+                    </ListItem>
+                </a>
+                <Divider className="divider"/>
 
-                    <a href="https://adamjwright.com/blog">
-                        <ListItem id="blog-link">
-                            <p id="blog-link">Blog</p>
-                        </ListItem>
-                    </a>
-                    <Divider className="divider"/>
-                    <a href="https://adamjwright.com/blog/contact_me">
-                        <ListItem id="contact-me-link">
-                            <p id="contact-me-link">Contact Me</p>
-                        </ListItem>
-                    </a>
-                    <Divider className="divider"/>
+            </List>
+        </div>
+    );
 
-                </List>
+    return (
+        <>
+            <div onClick={toggleDrawer('left', true)} id="menu-container">
+                <img src={menu} alt="menu" className={'menu'} />
+                <p id="menu-text">Menu</p>
             </div>
-        );
 
-        return (
-            <div>
-                <div onClick={this.toggleDrawer('left', true)} id="menu-container">
-                    <img src={menu} alt="menu" className={'menu'} />
-                    <p id="menu-text">Menu</p>
+            <Drawer open={left} onClose={toggleDrawer('left', false)}>
+                <div
+                    tabIndex={0}
+                    role="button"
+                    onClick={toggleDrawer('left', false)}
+                    onKeyDown={toggleDrawer('left', false)}
+                >
+                    {sideList}
                 </div>
-                <Drawer open={this.state.left} onClose={this.toggleDrawer('left', false)}>
-                    <div
-                        tabIndex={0}
-                        role="button"
-                        onClick={this.toggleDrawer('left', false)}
-                        onKeyDown={this.toggleDrawer('left', false)}
-                    >
-                        {sideList}
-                    </div>
-                </Drawer>
-            </div>
-        );
-    }
+            </Drawer>
+        </>
+    );
 }
 
 export default TemporaryDrawer;
